fbcd526fdf92d22698a7141507937d7b774111fe
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-07 20:34:36 +0700

    node-fetch

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const fetch = require("node-fetch");
 const cors = require("cors");
 const vkAuth = require("./config");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 
 app
-  .get("/", (_req, res) => res.send("Апи работает абсолютно корректно"))
+  .get("/", (_req, res) => res.send("Апи работает"))
   .get("/token", (req, res) => {
     const code = req.query.code;
 
@@ -16,6 +17,8 @@ app
       `${vkAuth.url}access_token?client_id=${vkAuth.client_id}&client_secret=${process.env.VK_SECRET}&redirect_uri=${vkAuth.redirect_uri}&code=${code}`
     )
       .then((data) => data.json())
-      .then((json) => res.json({ token: json.token, user_id: json.user_id }));
+      .then((json) =>
+        res.json({ access_token: json.access_token, user_id: json.user_id })
+      );
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
