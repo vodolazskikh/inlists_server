43b04b9ff49992c78fecbcd7b0f9e58a43eef41c
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-07 20:20:10 +0700

    cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
-express()
-  .get("/", (req, res) => res.send("Апи работает корректно"))
+const app = express();
+
+app.use(cors());
+
+app
+  .get("/", (req, res) => res.send("Апи работает корректно жи есть"))
   .get("/token", (req, res) => {
     const code = req.query.code;
     res.send(`Ты четко отработал, code=${code}`);
